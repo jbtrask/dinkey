605aa3b6751987fd5b7982a782611365165c794e
James Trask <pair+james@pivotallabs.com>
2012-05-17 23:42:53 -0700

    first version integrated rails and html5 boilerplate

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Dinkey::Application.routes.draw do
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
 
+  resources :people
+
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
